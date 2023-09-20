7d614c0da74219e1b12ffbbbb99f03111c2662a7
nasms018 <cjm1s@naver.com>
2023-09-20 12:30:10 +0900

    파일 첨부까지

--- src/atom/AttachFile.js
+++ src/atom/AttachFile.js
@@ -6,28 +6,30 @@ export default function AttachFile({ onFileSelect = f => f }) {
   const allowedFileTypes = "image/*, audio/*, video/*, application/x-zip-compressed, text/*";
 
   const handleChooseFile = (e) => {
-    const files = e.target.files;
-    if (!files) return;
-
-    console.log(files);
-
-    for (let i = 0; i < files.length; i++) {
-      const { type } = files[i];
-      console.log(type);
-      const allowed = allowedFileTypes.split(", ");
-      const isAllow = allowed.filter(allow => type.match(allow)).length > 0;
-      console.log(isAllow);
-      if (!isAllow)
-        continue;
-      //내용을 읽어서 동일 내용은 막는 장치
-      let reader = new FileReader();
-      reader.readAsDataURL(files[i]);
+    const allowedTypeReg = allowedFileTypes.split(", ")
+    //허용목록 파일들
+    const 허용목록파일들 = Array.from(e.target.files).filter((file)=>{
+      const { type } = file;
+      //허용 목록 중 하나 이상 걸렸니
+      return allowedTypeReg.filter(regExp => type.match(regExp)).length > 0;
+    });
+    let collectionOfFileAndHeader = []; //파일과 헤더 모음집
+    허용목록파일들.forEach((file)=>{
+      console.log(file.name + "파일을 readAsDataURL로 내용 읽기 요청합니다.");
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
       reader.onload = (e) => {
-        const fileContents = e.target.result;
-        const header = fileContents.substring(0, 1000);
-        onFileSelect(files[i], header);
-      };
-    }
+        //console.log(file.name+"파일 내용 load 이후에..");
+        const header = e.target.result.substring(0, 1000);
+        
+        collectionOfFileAndHeader.push({file, header});
+        console.log("정보확인"+ 허용목록파일들.length + "개 중 "+ collectionOfFileAndHeader.length+"개 허용");
+        if(허용목록파일들.length === collectionOfFileAndHeader.length){
+          //다 모았으면 parent로 전달
+          onFileSelect(collectionOfFileAndHeader);
+        }
+      }
+    })
   }
 
   return <Form.Control
--- src/atom/AttachedFileList.js
+++ src/atom/AttachedFileList.js
@@ -4,49 +4,92 @@ import { Button, Form } from 'react-bootstrap';
 import AttachFile from './AttachFile';
 
 export default function AttachFileList({ writer }) {
+  const thumbnailRequestTarget = ["video", "image"];
   //검사 장치
   const [contentFilter, setContentFilter] = useState([]);
   //지금까지 선택한 파일 기억장치. 업로드 용도
   const [attachedFiles, setAttachedFile] = useState([]);
-  function onFileSelect(file, header) {
-    console.log(attachedFiles.length);
-    if (!contentFilter.includes(header)) {
-      setContentFilter([...contentFilter, header]);
-      setAttachedFile([...attachedFiles, file]);
-    }
-    console.log(contentFilter.includes(header));
+
+  function onFileSelect(finedAndHeaders) {
+    let files = [], headers = [];
+    finedAndHeaders.forEach(({ file, header }) => {
+      if (!contentFilter.includes(header) && !headers.includes(header)) {
+        files.push(file);
+        headers.push(header);
+      }
+    });
+    setContentFilter([...contentFilter, ...headers]);
+    setAttachedFile([...attachedFiles, ...files]);
   }
+
   //지정된 파일들을 axios로 Server로 올리기하면 썸네일로 첨부파일들을 목록으로 보여준다.
   const handleAttach = async (e) => {
     e.preventDefault();
-
     const formData = new FormData();
-    
     // 동일 key에 여러개 할당 가능?
     Array.from(attachedFiles).forEach((file) => {
+      console.log(file);
       formData.append("attachFiles", file);
     });
-
+    let listAttachFileDTO = [];
     try {
-      const response = await axios.post(`/upload_multi`, formData, 
-        {headers: {
-          "Content-Type": "multipart/form-data",
-          "x-auth-token": `${writer.accessToken}`}});
+      const response = await axios.post(`/upload_multi`, formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "x-auth-token": `${writer.accessToken}`
+          }
+        });
       alert("성공");
       console.log(response.data);
+
+      Array.from(response.data).forEach((attachFileDTO)=>{
+        if(thumbnailRequestTarget.includes(attachFileDTO.contentType)){
+          listAttachFileDTO.push(attachFileDTO);
+        }
+      });
     } catch (error) {
       alert("돌아가");
       console.log(error);
     }
+    console.log(listAttachFileDTO);
+    listAttachFileDTO.forEach((attachFileDTO)=>{
+      썸네일파일가져오기(attachFileDTO);
+    });
   }
 
+  const 썸네일파일가져오기 = async (attachFileDTO) => {
+    try {
+      const response = await axios.post(`/displayThumbnail`, attachFileDTO,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            "x-auth-token": `${writer.accessToken}`
+          }});
+          const imagebytes = await response.arraybuffer();
+          //let blob = new blob([imagebytes], { type: response.headers['content-type'] });
+          //let imageurl = createobjecturl(blob);
+
+
+
+      console.log("썸네일 파일 정보 읽기 성공");
+      //console.log(response.headers['content-type']); 
+      //console.log(response.data);
+    } catch (error) {
+      console.log("썸네일 파일 정보 읽기 실패");
+      console.log(error);
+    }
+  }
+
+
+
   return <Form.Group className="mb-3" >
-    <Form.Label htmlFor="username">첨부파일 : </Form.Label>
-    {attachedFiles.map(af=><span> | {af.name}</span>) }
+    <Form.Label htmlFor="username">첨부파일</Form.Label>
+    {attachedFiles.map(af => <span> | {af.name}</span>)}
     <AttachFile onFileSelect={onFileSelect} />
     <Button variant="primary" onClick={handleAttach}>
       첨부
     </Button>
   </Form.Group>
-  
+
 }
